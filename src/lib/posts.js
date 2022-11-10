ab0e0ced9eaa314135256efbaf7f9c0b2cfda337
Sebi <mdnssebastian@gmail.com>
2022-11-10 17:38:17 +0000

    Added Article and RelatedArticle components

---
+++ src/lib/posts.js
@@ -0,0 +1,26 @@
+async function load() {
+  const fetchedPosts = import.meta.glob("../pages/posts/*.md", { eager: true });
+
+  const getPost = async (key) => {
+    const url = key.replace("../pages/", "/").replace(".md", "/");
+    const awaitedPost = await fetchedPosts[key].default();
+    const item = { ...awaitedPost.props.frontmatter, url, key };
+    return item;
+  };
+
+  const mappedPosts = Object.keys(fetchedPosts).map((key) => {
+    const awaitedPost = getPost(key);
+    return awaitedPost;
+  });
+
+  const results = await Promise.all(mappedPosts);
+  return results;
+}
+
+let _posts;
+
+export async function getAllPosts() {
+  _posts = _posts || load();
+
+  return await _posts;
+}
