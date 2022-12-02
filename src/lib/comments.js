f50c8379a58aa74026f0a13b8e20c1f205b87679
msebi <28646632+msebi@users.noreply.github.com>
2022-12-02 22:24:38 +0100

    [dev] fixed comments fetcher

--- src/lib/comments.js
+++ src/lib/comments.js
@@ -1,5 +1,11 @@
 const { Octokit } = require("@octokit/rest");
 const { createTokenAuth } = require("@octokit/auth-token");
+const md = require("markdown-it")();
+const sanitizeHtml = require("sanitize-html");
+const dayjs = require("dayjs");
+const dayjsRelativeTimePlugin = require("dayjs/plugin/relativeTime");
+
+dayjs.extend(dayjsRelativeTimePlugin);
 
 exports.handler = async (event) => {
   const issueNumber = event.queryStringParameters.id;
@@ -22,6 +28,36 @@ exports.handler = async (event) => {
         }),
       };
     }
+
+    const response = await octokitClient.issues.listComments({
+      owner: process.env.GITHUB_USER_NAME,
+      repo: process.env.GITHUB_REPO,
+      issue_number: issueNumber,
+    });
+
+    const comments = response.data
+      // Show comments in chronological order (oldest comments first)
+      .sort((comment1, comment2) =>
+        comment1.created_at.localeCompare(comment2.created_at)
+      )
+      // Restructure the data so the client-side JS doesn't have to do this
+      .map((comment) => {
+        return {
+          user: {
+            avatarUrl: comment.user.avatar_url,
+            name: sanitizeHtml(comment.user.login),
+          },
+          datePosted: dayjs(comment.created_at).fromNow(),
+          isEdited: comment.created_at !== comment.updated_at,
+          isAuthor: comment.author_association === "OWNER",
+          body: sanitizeHtml(md.render(comment.body)),
+        };
+      });
+
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({ data: comments }),
+    };
   } catch (e) {
     console.log(e);
     return {
