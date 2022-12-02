069e3780abb496ce3016eca14b2cd87d51e8035e
msebi <28646632+msebi@users.noreply.github.com>
2022-12-02 19:17:35 +0100

    [dev] Added rate limit getter

--- src/lib/comments.js
+++ src/lib/comments.js
@@ -1,8 +1,34 @@
-exports.handler = async (event, context) => {
+const { Octokit } = require("@octokit/rest");
+const { createTokenAuth } = require("@octokit/auth-token");
+
+exports.handler = async (event) => {
   const issueNumber = event.queryStringParameters.id;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ issueNumber }),
-  };
+  try {
+    const auth = createTokenAuth(process.env.GITHUB_PERSONAL_ACCESS_TOKEN);
+    const { token } = await auth();
+    const octokitClient = new Octokit({ auth: token });
+
+    const { data: rateLimitInfo } = await octokitClient.rateLimit.get();
+    const remainingCalls = rateLimitInfo.resources.core.remaining;
+
+    console.log(`GitHub API requests remaining: ${remainingCalls}`);
+
+    if (remainingCalls === 0) {
+      return {
+        statusCode: 429,
+        body: JSON.stringify({
+          error: "Unable to fetch comments at this time. Check back later.",
+        }),
+      };
+    }
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Unable to fetch comments for this post.",
+      }),
+    };
+  }
 };
